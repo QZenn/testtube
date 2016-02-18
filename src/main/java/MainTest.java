74d90dbdb1c4640814880a0070a28021854f9ab5
Evgeny Pogodin <evgeny.pogodin@gmail.com>
2016-02-18 21:41:16 +0300

    Extracted yaru test to main.

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,34 +1,33 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.lang.String;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.Assert.fail;
-
 /**
  * Created by QZen on 16/02/16.
  */
 public class MainTest {
-    private static WebDriver driver;
-    private static String baseUrl;
-    private static boolean acceptNextAlert = true;
-    private static StringBuffer verificationErrors = new StringBuffer();
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
         //set up
-        driver = new FirefoxDriver();
-        baseUrl = "https://yandex.ru";
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "https://ya.ru";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         driver.get(baseUrl + "/");
+        WebElement searchTestField = driver.findElement(By.name("text"));
+        searchTestField.click();
+        searchTestField.clear();
+        searchTestField.sendKeys("test");
+        WebElement searchBotton = driver.findElement(By.className("button"));
+        searchBotton.click();
+        String title = driver.getTitle();
+        System.out.println(title);
 
         //tear down
         driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
     }
 }
